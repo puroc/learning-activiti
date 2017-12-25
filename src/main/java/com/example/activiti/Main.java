bae156c232228d1d264c6a6d62f970330b14eaf2
John Doe <johndoe@doebrothers.com>
2017-12-25 12:12:44 +0800

    1

---
+++ src/main/java/com/example/activiti/Main.java
@@ -0,0 +1,13 @@
+package com.example.activiti;
+
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+    }
+}
